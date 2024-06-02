1dc68fb74c6f3de6a6f77b34e799118415aa3410
Guillem Lefait <guillem@datamq.com>
2024-06-02 14:49:22 -0400

    clean

--- src/components/EditionFlatDom.js
+++ src/components/EditionFlatDom.js
@@ -5,43 +5,6 @@ import { TranscriptContext } from '@/components/TranscriptContext';
 import {findDifferences} from "@/components/DataStructure";
 
 
-
-export function GenerateDomFromData(childrenUUIDs) {
-    const {transcription, setTranscription} = useContext(TranscriptContext);
-    let parts = [];
-    childrenUUIDs.map((partUUID, index) => {
-        const part = transcription[partUUID];
-        if (part.kind == 'edition') {
-            let editedNode = React.createElement('span', {
-                "x-data-uuid": partUUID,
-                key: partUUID,
-                className: "" + (index == 0 ? " firstWord" : "")
-            }, part.content);
-            parts.push(editedNode);
-        } else {
-            const subwords = part.children.map(subwordUUID  => {
-                const subword = transcription[subwordUUID]
-                 const current_subword = React.createElement('span', {
-                     "x-data-uuid": subwordUUID,
-                     "x-data-p": subword.p,
-                     key: subwordUUID,
-                     className: "p" + (100 * subword.p.toFixed(1)),
-                    "x-data-ms-start": subword.ms_start,
-                    "x-data-ms-end": subword.ms_end
-                 }, subword.content);
-                 return current_subword;
-            });
-            let currentWord = React.createElement('span', {
-                key: partUUID,
-                "x-data-uuid": partUUID,
-                className: "word" + (index == 0 ? " firstWord" : "")
-            }, subwords);
-            parts.push(currentWord);
-        }
-    });
-    return parts;
-}
-
 export function GenerateDomTextFromData(childrenUUIDs) {
     const {transcription, setTranscription} = useContext(TranscriptContext);
     let parts = '';
@@ -66,45 +29,6 @@ export function GenerateDomTextFromData(childrenUUIDs) {
 }
 
 export function ReverseDataFromDom(lineNode) {
-    // 1. remove ux blocks
-    let interestingPart = Array.from(lineNode.children)
-        .filter(n => ! n.classList.contains('uxOnly'));
-
-    // 2. for each part, either its an edition or a word containing subwords
-    // TODO: can added text goes in another span ?
-    let reversedData = {"parts": []}
-    for (let i=0; i<interestingPart.length; i++) {
-        if (interestingPart[i].childElementCount == 0){
-            // edited value -> get uuid
-            let edition_node = {
-                "kind": "edition",
-                "content": interestingPart[i].innerText,
-                "uuid": interestingPart[i].getAttribute('x-data-uuid')
-            }
-            reversedData.parts.push(edition_node);
-        } else {
-            // word containing syllabes
-            let word_node = {
-                "kind": "word",
-                "uuid": interestingPart[i].getAttribute('x-data-uuid'),
-                "children": []
-            }
-            for (let j=0; j<interestingPart[i].childElementCount; j++){
-                let subword = {
-                    "uuid": interestingPart[i].children[j].getAttribute('x-data-uuid'),
-                    "content": interestingPart[i].children[j].innerText,
-                    //"ms_start": parseInt(interestingPart[i].children[j].getAttribute('x-data-ms-start')),
-                    //"ms_end": parseInt(interestingPart[i].children[j].getAttribute('x-data-ms-end')),
-                    //"p": parseFloat(interestingPart[i].children[j].getAttribute('x-data-p'))
-                }
-                word_node.children.push(subword);
-            }
-            reversedData.parts.push(word_node);
-        }
-    }
-    return reversedData;
-}
-export function ReverseDataFromDom2(lineNode) {
     // 1. remove ux blocks
     let interestingPart = Array.from(lineNode.children);
 
@@ -187,7 +111,7 @@ export function TranscriptionLineDom({uuid}) {
     const inputChangeHandler = (e) => {
         e.stopPropagation();
         console.log("TranscriptionLine > inputChangeHandler.");
-        const newDomData = ReverseDataFromDom2(e.target);
+        const newDomData = ReverseDataFromDom(e.target);
         const updates = IdentifyUpdatesFromReversedDataFromDom(uuid, newDomData, transcription);
         if (Object.keys(updates).length > 0) {
             console.log(updates);
@@ -198,7 +122,6 @@ export function TranscriptionLineDom({uuid}) {
             setTranscription(newTranscription);
         }
     }
-    const parts = GenerateDomFromData(transcription[uuid].children);
     const partAsDom = GenerateDomTextFromData(transcription[uuid].children);
     let x = (
        <p className="transcriptionLine"
@@ -209,14 +132,10 @@ export function TranscriptionLineDom({uuid}) {
             <span className="editableLine" contentEditable={true} spellCheck="false"
                   x-data-uuid={uuid}
                   onBlur={inputChangeHandler}
-                  //onFocus={onFocus}
-                  //onInput={inputChangeHandler}
                   onInput={changeNotificationHandler}
-
-                dangerouslySetInnerHTML={{__html: partAsDom}}>
+                  dangerouslySetInnerHTML={{__html: partAsDom}}>
             </span>
         </p>
     );
-    //x.getElementById(uuid).dangerouslySetInnerHTML(partAsDom);
     return x;
 }
